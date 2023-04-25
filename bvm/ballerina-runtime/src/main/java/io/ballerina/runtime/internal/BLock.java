d6146f45333e5899420ad21a71c2c1c830966910
warunalakshitha <warudanu@gmail.com>
2023-04-26 01:10:17 +0530

    Revert Nadeeshan change

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/BLock.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/BLock.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing,
-*  software distributed under the License is distributed on an
-*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-*  KIND, either express or implied.  See the License for the
-*  specific language governing permissions and limitations
-*  under the License.
-*/
+ *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package io.ballerina.runtime.internal;
 
 import io.ballerina.runtime.internal.scheduling.State;
@@ -29,9 +29,9 @@ import java.util.ArrayDeque;
  */
 public class BLock {
 
-    private volatile ArrayDeque<Strand> current;
+    private ArrayDeque<Strand> current;
 
-    private volatile ArrayDeque<Strand> waitingForLock;
+    private ArrayDeque<Strand> waitingForLock;
 
     public BLock() {
         this.current = new ArrayDeque<>();
@@ -41,7 +41,6 @@ public class BLock {
     public synchronized boolean lock(Strand strand) {
         if (isLockFree() || lockedBySameContext(strand)) {
             this.current.offerLast(strand);
-            strand.acquiredLockCount++;
             return true;
         }
 
@@ -55,8 +54,7 @@ public class BLock {
 
     public synchronized void unlock() {
         //current cannot be empty as unlock cannot be called without lock being called first.
-        Strand removedStrand = this.current.removeLast();
-        removedStrand.acquiredLockCount--;
+        this.current.removeLast();
         if (!waitingForLock.isEmpty()) {
             Strand strand = this.waitingForLock.removeFirst();
             strand.scheduler.unblockStrand(strand);
